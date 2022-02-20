bc2f917a9a30c81f7f37011b91115f31a1b039d2
Türker Karapetti <trkrkrl53@gmail.com>
2022-02-20 11:06:17 +0300

    düzeltme 3:  customer e userid  propertyeklendi

--- Entities/Concrete/Customer.cs
+++ Entities/Concrete/Customer.cs
@@ -12,6 +12,7 @@ namespace Entities.Concrete
     {
         [Key]//CUSTOMER PRİMARY KEY İSTEDİ - YUKARDAKİ COMPONENTMODEL.DATAANNPTATIONS  kütüphanesini kullandık
         public int CusID { get; set; }
+        public int UserID { get; set; }//database table a da ekledik
         public string CompanyName { get; set; }
 
 
